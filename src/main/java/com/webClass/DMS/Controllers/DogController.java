9a3e2a992a210fde0991ef3c32559b35e1ed7081
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-11-20 21:23:07 -0600

    Added the package

--- src/main/java/com/webClass/DMS/Controllers/DogController.java
+++ src/main/java/com/webClass/DMS/Controllers/DogController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
  */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,7 +32,7 @@ public class DogController {
 	DogRepository dogRepo;
 	@Autowired
 	TrainerRepository trainerRepo;
-	
+	@GetMapping("/")
 	@RequestMapping("dogHome")
 	public ModelAndView home() {
 		mv.setViewName("home");
